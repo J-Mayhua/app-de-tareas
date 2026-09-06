2aae15b30cf2d15fa368d0abf61ecd9183078dfc
JMayhua <2021141015@unh.edu.pe>
2026-09-05 22:02:22 -0500

    feat: instalacion del CORS

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,21 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import authRoutes from './src/routes/auth_routes.js';
 import taskRoutes from './src/routes/tasks.routes.js';
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json()); // ← nueva línea: habilita lectura de JSON en el body
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('API funcionando 🚀');
 });
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
